035601fc0f217952b73b90ab522b6192c7d3eba9
repl.it user <replituser@example.com>
2021-11-06 19:34:08 +0000

    project done

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
+const bodyParser = require('body-parser');
+
 const app = express();
 
+const urls = new Map();
+let lastId=0;
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -17,6 +22,28 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/shorturl/new', function(req, res) {
+  let url = req.body.url;
+  if(!url.match(/[a-z]+:\/\/[a-z\.]+/)) {
+    res.json({ error: 'invalid url' });
+    return;
+  }
+  dns.lookup((new URL(url)).hostname,(err,data)=>{
+    if(err) {
+      res.json({ error: 'invalid url' });
+    } else {
+      urls.set(++lastId,url);
+      res.json({ original_url : url, short_url : lastId});
+    }
+  });
+});
+
+app.get('/api/shorturl/:url', function(req, res) {
+  res.redirect(urls.get(Number(req.params.url)));
 });
 
 app.listen(port, function() {
